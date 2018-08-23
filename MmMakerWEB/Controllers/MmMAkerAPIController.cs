e36053585d3901ee0e24c0107fca6d11ce03141b
Kamil Kkorczak <kamil.korczak@gmail.com>
2018-08-23 22:43:09 +0200

    Html

--- MmMakerWEB/Controllers/MmMAkerAPIController.cs
+++ MmMakerWEB/Controllers/MmMAkerAPIController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using MmMaker.Model;
+using MmMakerExcelManager;
 
 
 namespace MmMakerWEB.Controllers
@@ -13,11 +16,21 @@ namespace MmMakerWEB.Controllers
     public class MmMAkerAPIController : ApiController
     {
         [HttpPost]
-        public void Transformfile()
+        public List<ExcelContent> Transformfile()
         {
+            if (HttpContext.Current.Request.Files.Count == 0)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Plik jest wymagany."));
+            }
 
+            Stream filename = HttpContext.Current.Request.Files[0].InputStream;
+
+            ExcelParser mmMaker = new ExcelParser(filename);
+            List<ExcelContent> parsedContent = mmMaker.ParseExcel();
+
+            return parsedContent;
         }
 
-   
+
     }
 }
