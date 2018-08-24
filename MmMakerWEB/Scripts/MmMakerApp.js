d41d6fa05bd3a36d10c08420ddf8db43f975c16d
Kamil Kkorczak <kamil.korczak@gmail.com>
2018-08-24 22:59:44 +0200

    List of products

--- MmMakerWEB/Scripts/MmMakerApp.js
+++ MmMakerWEB/Scripts/MmMakerApp.js
@@ -1,5 +1,26 @@
 ﻿var app = angular.module("MmMakerApp", []);
 
-app.controller("AppController", function ($scope) {
- 
+
+app.controller("AppController",  function ($scope, $http) {
+    $scope.excelContent = [];
+    $scope.loadFile = function (files) {
+        var file = files[0];
+
+        var fd = new FormData();
+        fd.append("file", file);
+
+        $http({
+            url: '/api/mmmakerapi/Transformfile',
+            method: 'POST',
+            headers: { 'Content-Type': undefined },
+            data: fd
+        }).then(function (result) {
+            $scope.excelContent = $scope.excelContent.concat(result.data);
+        });
+    }
+    $scope.sendExcelContent = function () {
+
+    }
 });
+
+
